91c3f813dcce61a2ebcd32c24146e1b2eb3daa80
sneha390 <sneha39yadav@gmail.com>
2023-12-23 23:38:22 +0530

    Pattern

---
+++ solidRectangle.java
@@ -0,0 +1,13 @@
+package pattern;
+public class solidRectangle {
+    public static void main(String[] args) {
+        int numberOFRows = 5;
+        int numberOfColumns = 5;
+        for(int i = 0; i < numberOFRows; i ++){
+            for(int j = 0; j < numberOfColumns; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
