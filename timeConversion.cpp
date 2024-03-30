e653b9769c7e77fee0a3542feaf0d8309259ec55
sneha390 <sneha39yadav@gmail.com>
2024-03-30 21:11:24 +0530

    Time Conversion

--- timeConversion.cpp
+++ timeConversion.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 string timeConversion(string s) {
-    if(s[8] == 'A'){
-        if(s[0] == '1' && s[1] == '2'){
-            s[0] = s[1] = '0';
+    string format="";
+    format += s[8];
+    format +=s[9];
+
+    string hr = "";
+    hr += s[0];
+    hr += s[1];
+
+    if(format == "PM" && hr != "12"){
+        unordered_map<int , int > mp;
+
+        int count = 13;
+        for(int i = 1 ; i <= 11 ; i++){
+            mp[i] = count;
+            count++;
         }
+    
+        // stoi (String to integer)
+        int x = stoi(hr); 
+        int temp = mp[x];
+        string newhr = to_string(temp);
+        s[0] = newhr[0];
+        s[1] = newhr[1];    
+
     }
-    else{
-        if(s[1] == '8'){
-            s[0] = '2';
-            s[1]  = '1';
+    
+    else if(format == "AM" && hr == "12"){
+            s[0] = '0';
+            s[1] = '0';
         }
-        else if(s[1] == '9'){
-            s[0] = '2';
-            s[1] = '1';
-        }
-        else if(!(s[0] == '1' && s[1] == '2')){
-            s[0] += 1;
-            s[1] += 2;
-        }
+
+        
+        return s;
     }
-    s.erase(8,2);
-    return s;
+
     
 
-}
+
 
 int main()
 {
