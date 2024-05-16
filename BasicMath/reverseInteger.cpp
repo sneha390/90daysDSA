09878a9da6081cc3342e0acf220faa60c90c3e24
sneha390 <sneha39yadav@gmail.com>
2024-05-16 11:18:03 +0530

    ReverseINteger

---
+++ BasicMath/reverseInteger.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverse(int x){
+    int ans = 0;
+    while(x != 0){
+        if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)){
+                return 0;
+        }
+        ans = (ans * 10) + x % 10;
+        x = x /10;
+    }
+    return ans;
+}
+
+int main(){
+    int x ;
+    cin >> x;
+    int ans = reverse(x);
+    cout << ans << endl;;
+    return 0;
+}
