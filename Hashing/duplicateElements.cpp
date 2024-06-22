78dd9fa4a63177b5633c6f90dcb3085f4e550104
sneha390 <sneha39yadav@gmail.com>
2024-06-22 11:03:04 +0530

    checkDuplicateElementsWithinK

---
+++ Hashing/duplicateElements.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkDuplicateWithinK(int arr[] , int n , int k){
+
+    for( int i = 0 ; i < n; i++){
+        for(int j = i+1 ; j <=i+k && j < n ; j++){
+            if(arr[i] == arr[j]){
+                if((j-i) <= k){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+};
+
+int main(){
+    int arr[7]= { 10 , 5 , 3 , 4,3,5,6};
+    bool result = checkDuplicateWithinK(arr, 7 , 3);
+    if(result == true){
+        cout << "true" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+    return 0;
+}
