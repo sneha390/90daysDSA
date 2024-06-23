a8043a6db31927b2248547d43d5a0cf8020d0d66
sneha390 <sneha39yadav@gmail.com>
2024-06-23 23:16:15 +0530

    MinmumOPerationsToMakeDivisible

--- contest/notDividible.cpp
+++ contest/notDividible.cpp
@@ -1,3 +1,9 @@
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+// You are given an integer array nums. 
+// In one operation, you can add or subtract 1 from any element of nums.
+// Return the minimum number of operations to make all elements of nums divisible by 3.
+
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
