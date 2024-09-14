0ee463a9a77c86a064eb8eae6f86c7be66c702ab
sneha390 <sneha39yadav@gmail.com>
2024-09-14 09:41:01 +0530

    Optimized Query

--- Hashing/optimizedQuery.cpp
+++ Hashing/optimizedQuery.cpp
@@ -4,31 +4,26 @@ using namespace std;
 
 int main(){
     int n;
-    cout << "Enter the number of elements in array :";
+    cout << "Enter the number of elements" << endl;
     cin >> n;
     int arr[n];
     int hash[51] = {0};
-
-    cout << "Enter the elements of array"<<endl;
-    
-    for(int i = 0 ; i < n; i++){
+    cout << "Enter the elements in the array"<< endl;
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
         hash[arr[i]] = hash[arr[i]] + 1;
     }
-
     int q;
-    cout <<"Enter the number of query ";
+    cout << "Enter the number of query" << endl;
     cin >> q;
-
-    for(int i = 0 ; i < q; i++){
+    for(int i = 0; i< q ; i++){
         int query;
-        cout << "Enter the query "<< endl;
         cin >> query;
-
         int count = hash[query];
-        cout << query << " = " << count << endl;
+        cout << query << "==" << count << endl;
     }
 }
+
 
 // Time Complexity = O(n + q)
 // Space complexity = numbe rof elemments in hash and array
